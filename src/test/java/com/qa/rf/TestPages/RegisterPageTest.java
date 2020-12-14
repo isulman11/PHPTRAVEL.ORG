8f042f9db2670c93ba6d0fe266111c8cdc451d52
MacBook Pro <macbookpro@MacBooks-MBP.fios-router.home>
2020-12-14 00:54:33 -0500

    welcome to Data Driver

--- src/test/java/com/qa/rf/TestPages/RegisterPageTest.java
+++ src/test/java/com/qa/rf/TestPages/RegisterPageTest.java
@@ -4,16 +4,21 @@ import java.io.IOException;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.qa.rf.Base.TestBase;
 import com.qa.rf.Pages.HomePage;
 import com.qa.rf.Pages.RegisterPage;
+import com.qa.rf.Util.ReadExcel;
 
 public class RegisterPageTest extends TestBase {
 
 	HomePage homepage;
 	RegisterPage registerpage;
+	ReadExcel excel;
+
+	String sheetname = "sheet1";
 
 	public RegisterPageTest() throws IOException {
 		super();
@@ -34,12 +39,23 @@ public class RegisterPageTest extends TestBase {
 		System.out.println("Title of the Page  ===>>  " + title);
 		Assert.assertEquals(title, "Register - PHPTRAVELS");
 	}
-	
-	@Test
-	public void Enterinfo() throws InterruptedException {
-		registerpage.registeruser("Ali", "Hassan", "alihassan@icloud.com", "3012011011",
-				"123 Test Driver", "Hanover", "Maryland", "21076", "United States", "3012011011", "Helloworld", "Helloworld");
+
+	@DataProvider
+	public Object[][] getdata() {
+		Object data[][] = ReadExcel.getTestData(sheetname);
+		return data;
+
+	}
+
+	@Test(dataProvider = "getdata")
+	public void Enterinfo(String firstname, String lastname, String email, String phone, String address, String city,
+			String state, String postcode, String count, String whatsappNo, String password, String confirmpassword) throws InterruptedException {
+		registerpage.registeruser(firstname, lastname, email, phone, address, city, state, postcode, count, whatsappNo, password, confirmpassword);
+
 		
+//		registerpage.registeruser("Ali", "Hassan", "alihassan@icloud.com", "3012011011",
+//				"123 Test Driver", "Hanover", "Maryland", "21076", "United States", "3012011011", "Helloworld", "Helloworld");
+//		
 	}
 
 	@Test
